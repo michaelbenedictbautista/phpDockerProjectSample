ce1110c7636bbf15e57f0e6405b1a905bc120b1f
Mike <graphicalevolution@gmail.com>
2022-03-19 02:32:29 +1100

    book.php query getItems()

--- www/src/Book.php
+++ www/src/Book.php
@@ -5,7 +5,6 @@ use textreview\Database;
 use Exception;
 
 class Book extends Database {
-<<<<<<< HEAD
 
     private $dbconnection;
 
@@ -22,16 +21,22 @@ class Book extends Database {
     // execute a query
     public function getItems()
     {
-        $query = "SELECT 
-        book_id, 
+      $query = "
+      SELECT 
+        book.book_id,
         book_title,
         tagline,
         isbn13,
-        isbn10,
         year,
         pages,
-        created
-        FROM book";
+        book_image.image_id,
+        image.file_name AS picture
+        FROM book 
+        INNER JOIN book_image
+        ON book.book_id = book_image.book_id
+        INNER JOIN image
+        ON book_image.image_id = image.image_id 
+      ";
 
         try 
         {
@@ -67,74 +72,10 @@ class Book extends Database {
         }
     }
 }
-?>
-=======
-  private $dbconnection;
-  public function __construct()
-  {
-    // call the __construct() method of the Database class to initialise
-    parent::__construct();
-    // get the connection after it's been created and keep a reference in 
-    // a local variable called $db_connection
-    $this -> dbconnection = parent::getConnection();
-  }
 
-  public function getItems() 
-  {
-    $query = "
-    SELECT 
-      book.book_id,
-      book_title,
-      tagline,
-      isbn13,
-      year,
-      pages,
-      book_image.image_id,
-      image.file_name AS picture
-      FROM book 
-      INNER JOIN book_image
-      ON book.book_id = book_image.book_id
-      INNER JOIN image
-      ON book_image.image_id = image.image_id
-      WHERE 1
-    ";
+// public function getDetail() {
 
-    try
-    {
-      $statement = $this -> dbconnection -> prepare( $query );
-      if( !$statement ) 
-      {
-        throw new Exception("problem with query " . $query );
-      }
-
-      if( !$statement -> execute() ) 
-      {
-        throw new Exception("query failed to execute");
-      }
-      else 
-      {
-        $books = array();
-        $items = array();
-        $result = $statement -> get_result();
-        while( $row = $result -> fetch_assoc() ) 
-        {
-          array_push( $items, $row );
-        }
-        $books["total"] = count($items);
-        $books["items"] = $items;
-
-        return $books;
-      }
-      return null;
-    }
-    catch ( Exception $exception ) 
-    {
-      echo $exception -> getMessage();
-    }
-  }
-
-  public function getDetail() {
     
-  }
-}
->>>>>>> edf5e036a018e5fd72a0e902f9f80b3ee38ee623
+// }
+ 
+?>
--- www/test.php
+++ www/test.php
@@ -1,34 +1,8 @@
 <?php
-<<<<<<< HEAD
-   //enable comp[oser autoloading
-//    require("vendor/autoload.php");
-   
-//    // request classes from autoloader
-//    use textreview\Book;
-
-//    $book = new Book();
-//    $items = $book -> getItems();
-
-//    if ( $items ) 
-//    {
-//        print_r($items);
-//    }
-//    else 
-//    {
-//        echo "No books found!";
-//    }
-
-// Week 6 loading the result of rows to using twig library
-// enable composer autoloading
-require("vendor/autoload.php");
-
-//request namespace(textreview) and classes(Book) from autoloader
-=======
 // enable composer autoloading
 require("vendor/autoload.php");
 
 //request classes from autoloader
->>>>>>> edf5e036a018e5fd72a0e902f9f80b3ee38ee623
 use textreview\Book;
 
 $book = new Book();
@@ -36,15 +10,6 @@ $items = $book -> getItems();
 
 $loader = new \Twig\Loader\FilesystemLoader("templates");
 $twig = new Twig\Environment( $loader, [ "cache" => false ] );
-<<<<<<< HEAD
-echo $twig -> render("home.html.twig", ["page_title" => "TextReview", "greeting" => "Hello to my website",
-"books" => $items ] );
 
-
+echo $twig -> render("home.html.twig", ["page_title" => "TextReview", "greeting" => "Hello to my website", "books" => $items ] );
 ?>
-
-=======
-
-echo $twig -> render("home.html.twig", ["page_title" => "TextReview", "greeting" => "Hello there", "books" => $items ] );
-?>
->>>>>>> edf5e036a018e5fd72a0e902f9f80b3ee38ee623
